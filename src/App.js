bed76e3e16fcda2b8319371a57bd2db2a03a2802
Jesse Liu <42053831+jasssse@users.noreply.github.com>
2022-01-12 02:56:02 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { useState } from 'react'
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
 import './App.css';
+import TypingBox from './components/TypingBox';
+import StatBar from './components/StatBar';
+
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'left',
+  color: theme.palette.text.secondary,
+}));
 
 function App() {
+  var randomWords = require('random-words')
+  const [text, setText] = useState('')
+  // Replace with randomWords(number)
+  const [givenText, setGivenText] = useState(['Hello', 'World', 'LOL'])
+  const [correct, setCorrect] = useState(0)
+  const [mistakes, setMistake] = useState(0)
+  const [wpm, setwpm] = useState(0)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StatBar />
+      <h3>{givenText.join(' ')}</h3>
+      <h3>{text}</h3>
+      <TypingBox currentText={text} updateText={setText} />
     </div>
   );
 }
---
+++ src/components/StatBar.js
@@ -0,0 +1,11 @@
+function StatBar() {
+    // components for other stuff
+    return (
+        <div>
+            <h2>StatBar Here</h2>
+
+        </div>
+    )
+}
+
+export default StatBar
