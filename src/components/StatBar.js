b7068e2207af8db1857bac043e0309969444b63f
Jesse Liu <42053831+jasssse@users.noreply.github.com>
2022-01-16 11:36:04 -0500

    background colour tweak

--- src/components/StatBar.js
+++ src/components/StatBar.js
@@ -2,7 +2,7 @@ function StatBar({time, correct}) {
     // components for other stuff
     return (
         <div style={{display:'flex', justifyContent:"space-between", marginTop:'15px'}}>
-            <h1 style={{color:'#f44e3f', fontSize:"40px", marginTop:'0%'}}>{time}</h1>
+            <h1 style={{color:'#f44e3f', fontSize:"40px", marginTop:'0%'}}>{"time: "+time}</h1>
             <h1 style={{color:'#2c666e', fontSize:"40px", marginTop:'0%'}}>{"correct words: "+correct}</h1>
         </div>
     )
